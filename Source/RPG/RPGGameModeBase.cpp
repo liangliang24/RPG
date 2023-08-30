338b02b9e3837eb5d029e8e367b5cb4ec08c50ea
LLTouchingFish <62233176+liangliang24@users.noreply.github.com>
2023-08-30 20:00:42 +0800

    New Game Mode-Spawn AI

--- Source/RPG/RPGGameModeBase.cpp
+++ Source/RPG/RPGGameModeBase.cpp
@@ -3,3 +3,48 @@
 
 #include "RPGGameModeBase.h"
 
+#include "EnvironmentQuery/EnvQueryManager.h"
+
+ARPGGameModeBase::ARPGGameModeBase()
+{
+	spawnTimerInterval = 2.0f;
+}
+
+
+
+void ARPGGameModeBase::StartPlay()
+{
+	Super::StartPlay();
+
+	
+	GetWorldTimerManager().SetTimer(timerHandle_SpawnBots,this,&ARPGGameModeBase::SpawnBotTimerElasped,spawnTimerInterval,true);
+}
+
+
+
+
+void ARPGGameModeBase::SpawnBotTimerElasped()
+{
+	UEnvQueryInstanceBlueprintWrapper* result = UEnvQueryManager::RunEQSQuery(this,spawnBotQuery,this,EEnvQueryRunMode::RandomBest5Pct,nullptr);
+	if (ensure(result))
+	{
+		result->GetOnQueryFinishedEvent().AddDynamic(this,&ARPGGameModeBase::SpawnAIMinion);
+	}
+	
+}
+
+void ARPGGameModeBase::SpawnAIMinion(UEnvQueryInstanceBlueprintWrapper* EnvQueryInstanceBlueprintWrapper,
+ 	EEnvQueryStatus::Type Arg)
+{
+	if (Arg != EEnvQueryStatus::Success)
+	{
+		UE_LOG(LogTemp,Warning,TEXT("Spawn bots failed"));
+		return ;
+	}
+	TArray<FVector> locations = EnvQueryInstanceBlueprintWrapper->GetResultsAsLocations();
+
+	if (locations.Num() > 0)
+	{
+		GetWorld()->SpawnActor<AActor>(minion,locations[0],FRotator::ZeroRotator);
+	}
+}
--- Source/RPG/RPGGameModeBase.h
+++ Source/RPG/RPGGameModeBase.h
@@ -3,9 +3,12 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "EnvironmentQuery/EnvQueryTypes.h"
 #include "GameFramework/GameModeBase.h"
 #include "RPGGameModeBase.generated.h"
 
+class UEnvQueryInstanceBlueprintWrapper;
+class UEnvQuery;
 /**
  * 
  */
@@ -13,5 +16,22 @@ UCLASS()
 class RPG_API ARPGGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
+public:
+	ARPGGameModeBase();
+	virtual void StartPlay() override;
+protected:
+	FTimerHandle timerHandle_SpawnBots;
+	float spawnTimerInterval;
+	
+	UPROPERTY(EditDefaultsOnly,Category="AI")
+	UEnvQuery* spawnBotQuery;
+
+	UPROPERTY(EditDefaultsOnly,Category="AI")
+	TSubclassOf<AActor> minion;
+
+	UFUNCTION()
+	void SpawnBotTimerElasped();
+	UFUNCTION()
+	void SpawnAIMinion(UEnvQueryInstanceBlueprintWrapper* EnvQueryInstanceBlueprintWrapper, EEnvQueryStatus::Type Arg);
 	
 };
