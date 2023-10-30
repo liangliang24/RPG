ae0300c0c87c6126575d1f0ed55b39f2e35b918f
LLTouchingFish <62233176+liangliang24@users.noreply.github.com>
2023-10-30 16:00:16 +0800

    Create RALS_AnimInterface

---
+++ Source/RPG/Private/ALS/RALS_AnimInterface.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ALS/RALS_AnimInterface.h"
+
+// Add default functionality here for any IRALS_AnimInterface functions that are not pure virtual.
