e00a6e1e23a5fca93ac08b206a763c0d36d6de83
LLTouchingFish <62233176+liangliang24@users.noreply.github.com>
2023-11-20 15:55:04 +0800

    Gideon R Ability

---
+++ Source/RPG/Public/Action/RAction_Gideon_R_Ability.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RAction.h"
+#include "RAction_Gideon_R_Ability.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RPG_API URAction_Gideon_R_Ability : public URAction
+{
+	GENERATED_BODY()
+	
+};
